e3bd1f1f84e66a9d5bc4e8f0021a409d3de4609a
Muhammad Latif Harkat <68332606+Latip176@users.noreply.github.com>
2022-09-24 15:23:33 +0700

    Update scrap.py

--- data/scrap.py
+++ data/scrap.py
@@ -1,4 +1,4 @@
-import requests as req, re, os
+import requests, re, os
 from bs4 import BeautifulSoup
 
 img,judul,genre,author,chapter,status = [],[],[],[],[],[]
@@ -17,7 +17,7 @@ class serachKomik(Main):
 
     def request(self,url):
         data.clear();img.clear();author.clear();judul.clear();genre.clear();chapter.clear();status.clear()
-        r = BeautifulSoup(req.get(url, headers={"user-agent":"chrome"}).text, "html.parser")
+        r = BeautifulSoup(requests.get(url, headers={"user-agent":"chrome"}).text, "html.parser")
         #####################################################
         #scraping BeautifulSoup
         for d in r.find_all("div", {"class":"row c-tabs-item__content"}):
