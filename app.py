dfbf18ebaa07bdab4affe7017786b2550630a41f
Muhammad Latif Harkat <68332606+Latip176@users.noreply.github.com>
2022-09-24 16:20:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ def getApi():
         key = request.args.get("judul")
         if(key):
             req = serachKomik(key).request("https://mgkomik.com/?s={}&post_type=wp-manga".format(key))
-            if(req['jumlah-hasil-pencarian']==0):
-                return jsonify({"data-info":"Hasil pencarian tidak ditemukan!","jumlah-hasil-pencarain":0})
-            else:
-                return jsonify(req)
+            return jsonify(req)
         else:
             return jsonify({"error":"Search dulu bang, contoh /api/komik/?judul=test"})
     else:
